932a96e922e3d33a4c423b086ccd825fafccf606
Grover Cristobal <dfrontend@medialabla.com>
2023-09-20 07:39:16 -0500

    leer datos del curso seleccionado

--- 1. Proyecto-Carrito/js/app.js
+++ 1. Proyecto-Carrito/js/app.js
@@ -18,6 +18,22 @@ function agregarCurso (e) {
     // aqui usamos preventDefault para eliminar el comportamiento x defecto ya q los anchor al no encontrar un id o selector direcionan hacia arriba
     e.preventDefault()
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log(e.target)
+        const cursoSeleccionado = e.target.parentElement.parentElement
+        leerDatosCurso(cursoSeleccionado)
     }
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la info del curso
+function leerDatosCurso (curso) {
+    console.log(curso)
+
+    // aquí creamos un Objeto con el contenido del curso actual
+    const infoCurso = {
+        id: curso.querySelector('.agregar-carrito').dataset.id,                                                                       
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        cantidad: 1
+    }
+    console.log(infoCurso)
 }
